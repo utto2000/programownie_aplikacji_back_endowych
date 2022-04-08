1d3516618cc15ef8d38dab1bf7495c42ddc5e78b
utto2000 <bartek200039@gmail.com>
2022-04-09 01:05:37 +0200

    Add maping

--- lab2/demo/src/main/java/com/wator/lab2/UserEntity.java
+++ lab2/demo/src/main/java/com/wator/lab2/UserEntity.java
@@ -2,4 +2,17 @@ package com.wator.lab2;
 
 public class UserEntity {
 
+        private String name;
+
+        public UserEntity(String name){
+            this.name = name;
+        }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- lab2/demo/src/main/java/com/wator/lab2/UsersController.java
+++ lab2/demo/src/main/java/com/wator/lab2/UsersController.java
@@ -1,21 +1,49 @@
 package com.wator.lab2;
 
+import org.apache.catalina.User;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.AbstractMap;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.Map;
 
-@Controller
+import static java.util.Map.entry;
+
+@RestController
 public class UsersController {
-private Map<Integer, UserEntity> map;
+//private  Map<Integer, UserEntity> map = Map.ofEntries(entry(0, new UserEntity("Kamil")),entry(1, new UserEntity("Radek")));
+private  Map<Integer, UserEntity> map = new HashMap<Integer, UserEntity>();
+
+
 
     @GetMapping("/users")
+    public Map<Integer, UserEntity>  getUsers(){
+        map.put(0,new UserEntity("Radek"));
+        map.put(1,new UserEntity("WhoAsked"));
+
+        return  map;
+    }
+
+
+    @GetMapping("/users/{id}/get")
+    UserEntity getUser(@PathVariable int id){
+        return map.get(id);
+    }
+
+    @GetMapping("/users/{id}/remove")
+    UserEntity  removeUser(@PathVariable int id) {
+        return  map.remove(id);
+    }
+
+    @GetMapping("/users/add")
     @ResponseBody
-    public Collection<UserEntity> index(){
-        return map.values();
+    UserEntity addNewUser(@RequestParam String name) {
+        UserEntity user = new UserEntity(name);
+//        Map.Entry<Integer, UserEntity> entry = new AbstractMap.SimpleEntry<Integer,UserEntity>(map.size(),user);
+        map.put(map.size(), user);
+        return user;
     }
 
 }
